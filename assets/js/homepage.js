27c2678bf398c406b59183ab928a9fb7ec30ca03
Brian <bgswilde@gmail.com>
2021-08-03 00:23:35 -0500

    second api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -25,11 +25,11 @@ var getPlaces = function(searchCity) {
     // clear array
     placesArray = [];
     // fetch the city name from search to get coordinates(fix link below to reflect)
-    fetch("https://api.opentripmap.com/0.1/en/places/geoname?name=" + searchCity + "&apikey=" + apiKey)
+    fetch("https://api.geoapify.com/v1/geocode/search?text=" + searchCity + "&lang=en&limit=1&type=city&apiKey=5f14cd024f004280af18302ff6db6a1f")
         .then(response => (response.json())
             // take coordinates from previous fetch data to get places array
         .then(data => {
-            fetch("https://api.opentripmap.com/0.1/en/places/radius?radius=35000&lon=" + data.lon + "&lat=" + data.lat + "&rate=3&kinds=natural&limit=10&format=json&apikey=" + apiKey)
+            fetch("https://api.opentripmap.com/0.1/en/places/radius?radius=35000&lon=" + data.features[0].properties.lon + "&lat=" + data.features[0].properties.lat + "&rate=3&kinds=natural&limit=10&format=json&apikey=" + apiKey)
                 .then(response => (response.json())
                 .then(data => {
                     // for loop to push data to placesArray
@@ -42,64 +42,15 @@ var getPlaces = function(searchCity) {
                         .then(data => {
                             console.log(data)
                             placesArray.push(data)
+                            return;
                         })
                     };
+                    createCards()
                 }))
                 
         }))
-        // creating cards
-        Promise.console.log("this was reached");
-        for (var i = 0; i < placesArray.length; i++) {
-            // create a column div to hold the card
-            var columnDiv = document.createElement("div");
-            columnDiv.classList = "column is-one-fifth";
-            cardContainer.appendChild(columnDiv);
-    
-            // create a card to hold data
-            var placeCard = document.createElement("div");
-            placeCard.classList = "card";
-            columnDiv.appendChild(placeCard);
-    
-            // create a title for each card
-            var placeTitle = document.createElement("p");
-            placeTitle.classList = "title is-4";
-            placeTitle.textContent = placesArray[i].name;
-    
-            // create an image for each card
-            var placeImage = document.createElement("figure");
-            var imageData = document.createElement("img");
-            placeImage.classList = "image is-4by3";
-            imageData.setAttribute("src", "'" + placesArray[i].image + "'");
-            imageData.setAttribute("alt", "‘image showing a view of " + placesArray[i].name + "'");
-            placeImage.appendChild(imageData);
-    
-            // create a description for each card
-            var placeText = document.createElement("p");
-            placeText.classList = "description";
-            placeText.textContent = placesArray[i].wikipedia_extracts.text;
-            
-            // create a card footer with save and more buttons
-            var footer = document.createElement("footer");
-            var saveBtn = document.createElement("a");
-            var moreBtn = document.createElement("a");
-            footer.classList = "card-footer";
-            saveBtn.classList = "card-footer-item";
-            moreBtn.classList = "card-footer-item";
-            saveBtn.setAttribute("href", "#");
-            moreBtn.setAttribute("href", "#");
-            footer.appendChild(saveBtn);
-            footer.appendChild(moreBtn);
-    
-    
-            // append children to card
-            placeCard.appendChild(placeTitle); 
-            placeCard.appendChild(placeImage);
-            placeCard.appendChild(placeText);
-            placeCard.appendChild(footer);
-          
-        }
 
-var createCards = function() {
+var createCards = function(placesArray) {
     console.log("this was reached");
     for (var i = 0; i < placesArray.length; i++) {
         // create a column div to hold the card
@@ -121,7 +72,7 @@ var createCards = function() {
         var placeImage = document.createElement("figure");
         var imageData = document.createElement("img");
         placeImage.classList = "image is-4by3";
-        imageData.setAttribute("src", "'" + placesArray[i].image + "'");
+        imageData.setAttribute("src", "'" + placesArray[i].preview.source + "'");
         imageData.setAttribute("alt", "‘image showing a view of " + placesArray[i].name + "'");
         placeImage.appendChild(imageData);
 
